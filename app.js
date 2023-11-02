a694657de2a44ecc3896bc63cef1242652fda15d
Patrick Annan <you@example.compatrickannan@hotmail.com>
2023-11-02 18:48:11 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+// 2. Create a function that returns the sum of two numbers that are arguments. Then console.log the function with arguments.
+
+function add(a, b) {
+  var sum = a + b;
+ }
+ console.log(sum)
+
+//  3. Create a function that returns the difference of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+
+function subtract(a, b) {
+  var difference = a - b;
+ }
+ console.log(difference)
+
+//  4. Create a function that returns two arguments multiplied by each other. Then console.log the function with the variables from step two as your two arguments.
+
+function multiply(a, b) {
+  var product = a * b;
+ }
+ console.log(product)
+
+//  Create a function that returns divides the first argument by the second argument and returns the new value. Then console.log the function with the variables from step two as your two arguments.
+
+function divide(a, b) {
+  var quotient = a / b;
+ }
+ console.log(quotient)
+
